2efc10cbcf36e51a738c21b366c989fde8c748a9
Ivan Sekularac <ivan.sekularac@hotmail.com>
2019-12-30 21:46:55 +0100

    Favorite functionality added, duration and popularity calcs added

--- music/models.py
+++ music/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class Playlist(models.Model):
     name = models.CharField(max_length = 100)
--- music/views.py
+++ music/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from music.models import Playlist, Song
 import requests
 import json
-
+import datetime
 
 #  Headers are used in every call and they are always the same
 headers = {
@@ -12,11 +12,20 @@ headers = {
     'x-rapidapi-key': "3060c38672msha6ca68fb91aa196p106ce5jsn9c6ba1778834"
 }
 
+# Perform Song duration calc
+def durationCalc(duration):
+    duration = str(datetime.timedelta(seconds = duration))
+    return duration[-5:]
+
+# Perform Popularity conversion to float number
+def popularityCalc(value):
+    return str(round(int(value) / 100000, 1))
+
 # Creating a View for Playlists Page
 def playlists_view(request):
     user = request.user
     playlists = Playlist.objects.filter(created_by = user)
-    return render(request, 'music/playlists.html', {'data' : playlists})
+    return render(request, 'music/playlists.html', { 'data' : playlists })
 
 # Creating a View for Favorites Page
 def favorites_view(request):
@@ -31,7 +40,7 @@ def favorites_view(request):
         response = requests.request("GET", url, headers=headers).json()
         data.append(response)
     # Render Template and pass the data to it
-    return render(request, 'music/favorites.html', {'data': data })
+    return render(request, 'music/favorites.html', { 'data': data })
 
 # Creating a View for Browse Page
 def browse_view(request):
@@ -47,7 +56,7 @@ def search_view(request):
         url = "https://deezerdevs-deezer.p.rapidapi.com/search"
         querystring = {"q": query}
 
-        response = requests.request("GET", url, headers=headers, params=querystring).json()
+        response = requests.request("GET", url, headers = headers, params = querystring).json()
         data.update(response)
 
         # We need all results since there is 25 per call, we iterate until we got all of them in data dict
@@ -60,6 +69,12 @@ def search_view(request):
 
             for k in response['data']:
                 data['data'].append(k)
+
+    # Perform duration and popularity calc for each song
+    for song in data['data']:
+        song['duration'] = durationCalc(song['duration'])
+        song['rank'] = popularityCalc(song['rank'])
+
 
     # Creating or adding tracks to Playlists
     if request.method == "POST":
@@ -84,17 +99,24 @@ def search_view(request):
             # Add Song object to Playlist object
             playlist_obj.songs.add(song_obj)
 
+
     # Fetch all User's Playlists for populating Add To feature
     user_playlists = Playlist.objects.filter(created_by = request.user)
+    # Pass favorite Songs
+    user_favorite_playlist = Playlist.objects.get(name = "My Favorites", created_by = request.user)
+    favorite_songs_objs = user_favorite_playlist.songs.all()
+    favorite_songs = []
+    for song in favorite_songs_objs:
+        favorite_songs.append(int(song.api_id))
 
     # Render template and pass the data we got so it can be used
-    return render(request, 'music/search.html', { 'results': data, 'user_playlists': user_playlists })
+    return render(request, 'music/search.html', { 'results': data, 'user_playlists': user_playlists, 'favorite_songs': favorite_songs })
 
 # Creating single view track by taking GET param as Track ID
 def track_view(request, id):
 
     url = "https://deezerdevs-deezer.p.rapidapi.com/track/" + str(id)
-    response = requests.request("GET", url, headers=headers).json()
+    response = requests.request("GET", url, headers = headers).json()
 
     return render(request, 'music/track.html', { 'data': response })
 
@@ -102,7 +124,7 @@ def track_view(request, id):
 def album_view(request, id):
 
     url = "https://deezerdevs-deezer.p.rapidapi.com/album/" + str(id)
-    response = requests.request("GET", url, headers=headers).json()
+    response = requests.request("GET", url, headers = headers).json()
 
     return render(request, 'music/album.html', { 'data': response })
 
@@ -110,7 +132,7 @@ def album_view(request, id):
 def artist_view(request, id):
 
     url = "https://deezerdevs-deezer.p.rapidapi.com/artist/" + str(id)
-    response = requests.request("GET", url, headers=headers).json()
+    response = requests.request("GET", url, headers = headers).json()
 
     return render(request, 'music/artist.html', { 'data': response })
 
@@ -133,8 +155,13 @@ def playlist_view(request, id):
     data = []
     for song in songs:
         url = "https://deezerdevs-deezer.p.rapidapi.com/track/" + str(song.api_id)
-        response = requests.request("GET", url, headers=headers).json()
+        response = requests.request("GET", url, headers = headers).json()
         data.append(response)
+
+    # Perform duration and popularity calc for each song
+    for song in data:
+        song['duration'] = durationCalc(song['duration'])
+        song['rank'] = popularityCalc(song['rank'])
 
     return render(request, 'music/playlist.html', { 'data': data })
 
@@ -142,15 +169,28 @@ def playlist_view(request, id):
 # Creating view for favoriting Songs
 def favorited_view(request):
     if request.method == "POST":
+        # Check if the song is in favorites already or no, based on that we will remove it or add
+        # to favorites on 'heart' button click
         # Save sent id
         song_id = request.POST.get('song_id')
         # Create song object
-        song_obj = Song.objects.create(api_id = song_id)
-        song_obj.save()
+        #song_obj = Song.objects.create(api_id = song_id)
+        # song_obj.save()
         # Add this song to users My Favorite Playlists
         playlist_obj = Playlist.objects.get(name = 'My Favorites', created_by = request.user)
         playlist_obj.save()
-        # Add song to this playlists
-        playlist_obj.songs.add(song_obj)
 
-    return HttpResponse('Favorited! <3') 
+        # Check if song is in playlist or no:
+        is_favorited = playlist_obj.songs.filter(api_id = song_id)
+
+        if len(is_favorited) > 0:
+            # Remove song from playlist because it's already in it
+            unvaforited_song = playlist_obj.songs.get(api_id = song_id)
+            unvaforited_song.delete()
+        else:
+            # Add song to this playlists because it's not in it already
+            song_obj = Song.objects.create(api_id = song_id)
+            song_obj.save()
+            playlist_obj.songs.add(song_obj)
+
+    return HttpResponse('Favorited! <3')
